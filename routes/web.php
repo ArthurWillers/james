de1c003222e0857d7739c71a1224cfd2f96b95b6
Arthur Willers <arthurwillers@proton.me>
2026-06-09 02:00:13 -0300

    feat(routes): definir rotas autenticadas e registrar alias do middleware auth

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', '/login');
 
+Route::middleware('auth')->group(function () {
+    Route::view('/dashboard', 'dashboard')->name('dashboard');
+    Route::view('/settings', 'settings')->name('settings');
+});
