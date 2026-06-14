103813e3c13f7d03fa3b9b83dbba583e92b6c97f
ArthurWillers <arthurwillers@proton.me>
2026-06-13 22:39:43 -0300

    feat: add migrations to enable unaccent and pg_trgm PostgreSQL extensions

---
+++ database/migrations/0001_01_01_000000_create_extensions.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::statement('CREATE EXTENSION IF NOT EXISTS unaccent');
+        DB::statement('CREATE EXTENSION IF NOT EXISTS pg_trgm');
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::statement('DROP EXTENSION IF EXISTS pg_trgm');
+        DB::statement('DROP EXTENSION IF EXISTS unaccent');
+    }
+};
