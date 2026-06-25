777e6c8ba4c91185489fd8f09883b42bce105c6f
ArthurWillers <arthurwillers@proton.me>
2026-06-25 18:18:04 -0300

    feat(finance): create radio block components

---
+++ resources/views/components/form/radio-block.blade.php
@@ -0,0 +1,20 @@
+@props([
+    'name',
+    'value',
+    'icon' => null,
+    'label' => '',
+    'activeClass' => 'peer-checked:text-neutral-900',
+    'inactiveClass' => 'text-neutral-500 hover:text-neutral-700',
+])
+
+<label class="flex-1 h-full">
+    <input type="radio" name="{{ $name }}" value="{{ $value }}" {{ $attributes->except(['class']) }} class="peer sr-only" />
+    <div class="flex flex-col h-full items-center justify-center py-3 rounded-lg transition-all cursor-pointer peer-checked:bg-white peer-checked:shadow-sm {{ $activeClass }} {{ $inactiveClass }} {{ $attributes->get('class') }}">
+        @if($icon)
+            <x-dynamic-component :component="$icon" class="w-6 h-6 mb-1" />
+        @endif
+        @if($label)
+            <span class="text-sm font-medium">{{ $label }}</span>
+        @endif
+    </div>
+</label>
