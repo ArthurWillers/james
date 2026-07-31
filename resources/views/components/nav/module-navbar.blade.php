587f8d5064a212400a1c406821791b7d4ac1df1c
Arthur Willers <153851227+ArthurWillers@users.noreply.github.com>
2026-07-31 17:59:17 -0300

    ui: update card border radii to rounded-xl and improve module navbar responsiveness for small screens (#21)

--- resources/views/components/nav/module-navbar.blade.php
+++ resources/views/components/nav/module-navbar.blade.php
@@ -17,7 +17,7 @@
 
 @props(['scrollable' => false])
 
-<div {{ $attributes->class('-mx-6 lg:-mx-8 px-6 lg:px-8 mb-6 border-b border-neutral-300 bg-neutral-100 -mt-6 lg:-mt-8') }}>
+<div {{ $attributes->class('-mx-3 sm:-mx-6 lg:-mx-8 px-3 sm:px-6 lg:px-8 mb-6 border-b border-neutral-300 bg-neutral-100 -mt-3 sm:-mt-6 lg:-mt-8') }}>
     <nav @class([
         'flex items-center gap-0.5 py-3',
         'overflow-x-auto overflow-y-hidden' => $scrollable,
