57b10a48198b7f3cd20e1e59416b2ee6185336de
Arthur Willers <arthurwillers@proton.me>
2026-08-22 15:47:27 -0300

    feat: add sliding animation component for radio block groups and include test discovery helper script

--- resources/views/components/form/radio-block-group.blade.php
+++ resources/views/components/form/radio-block-group.blade.php
@@ -1,10 +1,16 @@
-@props(['legend' => ''])
+@props([
+    'legend' => '',
+    'sliding' => true,
+])
 
 <fieldset>
     @if($legend)
         <legend class="text-sm font-semibold text-neutral-700 mb-3">{{ $legend }}</legend>
     @endif
-    <div {{ $attributes->merge(['class' => 'grid grid-cols-2 sm:flex sm:flex-row gap-2 items-stretch bg-neutral-100 rounded-xl p-1']) }}>
+    <div {{ $attributes->merge(['class' => 'grid grid-cols-2 sm:flex sm:flex-row gap-2 items-stretch bg-neutral-200 rounded-xl p-1 ' . ($sliding ? 'radio-block-group--sliding' : '')]) }}>
+        @if($sliding)
+            <span class="radio-block-indicator bg-white" aria-hidden="true"></span>
+        @endif
         {{ $slot }}
     </div>
 </fieldset>
