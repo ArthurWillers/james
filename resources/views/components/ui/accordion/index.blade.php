332d2da64aef3547804f7ff44efe3bea639e5a0c
Arthur Willers <arthurwillers@proton.me>
2026-08-22 12:50:04 -0300

    refactor: update accordion component to support custom trigger buttons and migrate password rules to use the component

--- resources/views/components/ui/accordion/index.blade.php
+++ resources/views/components/ui/accordion/index.blade.php
@@ -1,6 +1,7 @@
 @props([
     'id' => null,
     'open' => false,
+    'headClass' => '',
     'panelClass' => '',
     'innerClass' => '',
 ])
@@ -10,12 +11,20 @@
 @endphp
 
 <div
+    {{ $attributes->merge(['class' => 't-acc']) }}
     x-data="{ open: @js($open), toggle() { this.open = !this.open; } }"
     data-open="{{ $open ? 'true' : 'false' }}"
     x-bind:data-open="open"
-    {{ $attributes->merge(['class' => 't-acc']) }}
 >
-    {{ $trigger }}
+    <button
+        type="button"
+        class="t-acc-head w-full cursor-pointer border-0 bg-transparent p-0 text-left focus:outline-none {{ $headClass }}"
+        aria-controls="{{ $accordionId }}"
+        x-bind:aria-expanded="open"
+        @click="toggle()"
+    >
+        {{ $trigger }}
+    </button>
 
     <div
         id="{{ $accordionId }}"
